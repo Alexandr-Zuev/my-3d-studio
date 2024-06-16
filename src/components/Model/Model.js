d9c784de12a0c90b5f2973b319490f53334e3b35
unknown <zuev.alexandr.d@yandex.ru>
2024-06-16 22:22:04 +0300

    Деплой на гит

--- src/components/Model/Model.js
+++ src/components/Model/Model.js
@@ -3,7 +3,7 @@ import { useLoader } from '@react-three/fiber';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
 
 function Model() {
-  const geometry = useLoader(STLLoader, '/stl/logo.stl');
+  const geometry = useLoader(STLLoader, process.env.PUBLIC_URL + '/stl/logo.stl');
   
   return (
     <mesh geometry={geometry} scale={[0.02, 0.02, 0.02]} position={[-3, 0, 0]}>
